2e2adb28f4fcdf7a030ba2051fbd3e1190ad3f32
Joseph <JosephSped23@gmail.com>
2023-09-28 15:31:33 -0400

    moves dodger left and right

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "arrowRight") {
+      moveDodgerRight();
+    }
+    
+});
+function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+           if (left > 0) {
+          dodger.style.left = `${left - 10}px`;
+        }
+}
+  
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 10}px`;
+    }
+}
